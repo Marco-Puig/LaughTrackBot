d8d122800fb18671d22f9a9a6b028155a87a8b4b
Marco Puig <dpuig2021@fau.edu>
2021-09-25 13:38:51 -0400

    Commit #1
    Bot.init();

---
+++ main.js
@@ -0,0 +1,26 @@
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const prefix = '!';
+
+client.once('ready', () => {
+    console.log('Time to start your sitcom!');
+});
+
+client.on('message', message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'help'){
+        message.channel.send('There are 3 commands: !laugh !laugh1 !laugh2');
+    } else if(command == 'laugh'){
+        message.channel.send('https://www.youtube.com/watch?v=iYVO5bUFww0&ab_channel=HollywoodLaughTracks')
+    } else if(command == 'laugh1'){
+        message.channel.send('https://www.youtube.com/watch?v=5gkOozXnDUw&ab_channel=SimplySoundEffects')
+    }
+});
+
+client.login('ODkxMzYwNjA4MTU3NTgxMzQy.YU9OQQ.i2WJIEXtK_ZJpCKb7ar-mpAcOwQ');
