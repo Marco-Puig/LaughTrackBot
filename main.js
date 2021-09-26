43264d8f6c13e99a235ab989cf31891e975a3c64
Marco Puig <dpuig2021@fau.edu>
2021-09-25 22:01:33 -0400

    Final Commity
    Was really fun learning and creating my first ever bot.
    Hope you all enjoy it :D

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const { Client, Intents } = require('discord.js');
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-const prefix = '!';
+const prefix = '';
 
 client.once('ready', () => {
     console.log('Time to start your sitcom!');
@@ -14,13 +12,24 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'help'){
-        message.channel.send('There are 3 commands: !laugh !laugh1 !laugh2');
-    } else if(command == 'laugh'){
-        message.channel.send('https://www.youtube.com/watch?v=iYVO5bUFww0&ab_channel=HollywoodLaughTracks')
-    } else if(command == 'laugh1'){
-        message.channel.send('https://www.youtube.com/watch?v=5gkOozXnDUw&ab_channel=SimplySoundEffects')
+    if(command === 'help')
+    {
+        message.channel.send('There are ZERO commands: it is all passive!');
+        message.channel.send('Make sure to say a good joke :D');
+    } 
+    else if(command.length >= 30)
+    {
+        message.channel.send('https://tenor.com/view/laughing-laughing-hysterically-lol-happy-funny-gif-17906572')
+    } 
+    else if(command.length >= 5)
+    {
+        message.channel.send('https://tenor.com/view/laughing-mike-tyson-thats-hilarious-clap-laugh-gif-16490572');
+    } 
+    else if(command.length <= 4)
+    {
+        message.channel.send('https://tenor.com/view/kirby-gif-23200676');
     }
 });
 
-client.login(TOKEN); //discord will not allow me to post my token :O
+client.login('TOKEN');
+
